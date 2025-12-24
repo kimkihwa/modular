5d71eac8dd5f3cd3a776c6e442f019cecbca53d3
kimkihwa <kimkihwa@example.com>
2025-12-24 13:33:20 +0900

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
+  base: '/modular/',
   plugins: [
     vue({
       template: { transformAssetUrls }
